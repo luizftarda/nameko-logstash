98081f1b1bbfa508c65852b4d334dd4fdddd42ba
Jefferson Daniel <jefferson.daniel@skyone.solutions>
2017-02-23 11:09:37 -0300

    Fix setup.py script and changed version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='nameko_logstash',
-    version='0.1',
+    version='0.2',
     packages=['nameko_logstash',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+    long_description='A logstash dependency injector for nameko',
 )
