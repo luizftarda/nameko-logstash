d57e43b786116a75cac47df8a6739d93ed048db5
Jefferson Daniel <jefferson.daniel@skyone.solutions>
2017-02-23 14:19:58 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='nameko_logstash',
-    version='0.2',
+    version='0.3',
     packages=['nameko_logstash',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description='A logstash dependency injector for nameko',
