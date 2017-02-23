bf8a6479b83d8d4191769b81f0801b6a6bb3933a
Jefferson Daniel <jefferson.daniel@skyone.solutions>
2017-02-23 14:19:33 -0300

    Adding local_file and formatter options

--- nameko_logstash/__init__.py
+++ nameko_logstash/__init__.py
@@ -1,12 +1,23 @@
 """
 Nameko Logstash Dependency Provider
 """
-from nameko.extensions import DependencyProvider
 import logging
 import logstash
 
+from nameko.extensions import DependencyProvider
+from logging import FileHandler
+
 
 class LogstashDependency(DependencyProvider):
+
+    def __init__(self, local_file=None, log_formatter=None, *args, **kwargs):
+
+        self.local_file = local_file
+        if not log_formatter:
+            log_formatter = ("%(asctime)s - %(name)s - "
+                             "%(levelname)s - %(message)s")
+        self.log_formatter = log_formatter
+        super(DependencyProvider, self).__init__(*args, **kwargs)
 
     def setup(self):
         config = self.container.config.get('LOGSTASH', {})
@@ -20,7 +31,16 @@ class LogstashDependency(DependencyProvider):
         worker_ctx = args[0]
         logger = logging.getLogger(worker_ctx.service_name)
         logger.setLevel(self.log_level)
-        logger.addHandler(
-            logstash.LogstashHandler(self.host, self.port, version=1))
+
+        handler = logstash.LogstashHandler(self.host, self.port, version=1)
+        formatter = logging.Formatter(self.log_formatter)
+
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        if self.local_file:
+            file_handler = FileHandler(self.local_file)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
         return logger
 
